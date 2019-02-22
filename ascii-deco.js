ff36168721cb8f62da85cf4d4cc3c8322237b2dc
akshay_saraf <akshay_saraf@persistent.com>
2019-02-22 15:23:57 +0530

    Added functions for triangle and square

--- ascii-deco.js
+++ ascii-deco.js
@@ -1,8 +1,38 @@
 
-exports.divider = function(character, length = 50){
-    var structure = '';
-    for(var i = 0; i < length;i++){
-        structure += character;
+// Returns a string of given characters of given length
+exports.lineBanner = function(character, length = 50){
+    var line = '';
+    for(var i = 0; i < length; i++){
+        line += character+' ';
     }
-    console.log(structure);
+    return line;
+}
+
+// Returns a triangle shape made up of given characters
+// length represents the number of characters in the triangle's base
+exports.triangle = function(character, length = 5){
+    var shape = '';
+    for(var i = 0; i < length; i++){
+        for(var j = length-i; j > 0;j--){
+            shape += ' ';
+        }
+        for(var k = 0; k <= i ;k++){
+            shape += character+' ';
+        }
+        shape += '\n';
+    }
+    return shape;
+}
+
+// Returns a square shape made up of given characters
+// length represents the number of characters in the square's sides
+exports.square = function(character, length = 5){
+    var shape = '';
+    for(var i = 0; i < length; i++){
+        for(var j = 0; j < length; j++){
+            shape += character+' ';
+        }
+        shape += '\n';
+    }
+    return shape;
 }
